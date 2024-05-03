a8425bd058536248119a988f3a0c3dd69c470777
Thamindu Navojith <it21489464@my.sliit.lk>
2024-05-04 03:05:44 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,93 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require('dotenv').config();
+require("dotenv").config();
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const PORT = process.env.PORT || 8070;
+ app.use(cors());
+ app.use(bodyParser.json());
 
-app.use(cors());
-app.use(bodyParser.json());
+ const URL= process.env.MONGODB_URL;
 
-const URL = process.env.MONGODB_URL;
+ mongoose.connect(URL, {
+   // useCreateIndex: true,
+   // useNewUrlParser: true,
+   //useUnifiedTopology: true,     
+   // useFindAndModify: false
+ });
 
-mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+ const connection = mongoose.connection;
+ connection.once("open", () => {
+console.log("Mongodb Connection success!");
+ });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Mongodb connection success!');
-});
+//sammani
+ const storeRouter = require("./routes/stores.js");
+ app.use("/store",storeRouter);
 
-const paymentRouter = require("./routes/payments.js");
+ const shopRouter = require("./routes/shops.js");
+ app.use("/shop",shopRouter);
 
-app.use("/payment", paymentRouter);
+ const contactRouter = require("./routes/contacts.js");
+ app.use("/contact",contactRouter);
 
-app.listen(PORT, () => {
+ const adsRouter = require("./routes/ads.js");
+ app.use("/advertisment",adsRouter);
+
+ const bayleeRouter = require("./routes/baylee.js");
+ app.use("/baylee",bayleeRouter);
+
+ const cartRouter = require("./routes/cart.js");
+ app.use("/cart",cartRouter);
+
+ const reviewRouter = require("./routes/review.js");
+ app.use("/review",reviewRouter);
+
+ const orderRouter = require("./routes/order.js");
+ app.use("/order",orderRouter);
+
+ const peopleRouter = require("./routes/Users.js");
+app.use("/users", peopleRouter);
+
+const overviewRouter = require("./routes/overview.js");
+app.use("/overview", overviewRouter);
+
+const codeRouter = require("./routes/sales.js");
+app.use("/code", codeRouter);
+
+const tshirtRouter = require("./routes/tshirt.js");
+app.use("/Tee", tshirtRouter);
+
+
+//sandaru
+//create loyalty
+const createLoyaltyRouter = require("./routes/CreateLoyaltyRoutes");
+app.use("/createLoyalty",createLoyaltyRouter);
+
+//show loyalty membership to admin
+const loyaltyMembershipRouter = require("./routes/LoyaltyAdminWatchRoutes");
+app.use("/loyaltyAdminWatch",loyaltyMembershipRouter);
+
+//Add approved loyalty memberships 
+const approvedmembershipRouter = require("./routes/ApproveMembersRoutes");
+app.use("/approvedMemberships", approvedmembershipRouter);
+
+
+//sandali
+
+
+
+//ravindu
+
+
+
+
+ app.listen(PORT, () => {
     console.log(`Server is up and running on port number : ${PORT}`);
-});
+ });
+ 
