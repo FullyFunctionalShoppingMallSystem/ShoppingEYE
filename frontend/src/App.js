09fafc98364af959ba02d41cd2890ba24f77e9c6
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-05-03 08:50:38 +0530

    folder chnags commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import ContactUs from "./Sammani/Components/pages/ContactUs.js";
 import NavBar from "./Sammani/Components/NavBar.js";
-import ContactTable from "./Sammani/Components/AdminDashboard/ContactTable.js";
+import ContactTable from "./components/sammani/AdminDashboard/ContactTable.js";
 import Stores from "./Sammani/Components/pages/Stores.js";
 import Slider from "./Sammani/Components/pages/SliderFashion.js";
 import Cart from "./Sammani/Components/pages/Cart.js"
-import Order from "./Sammani/Components/AdminDashboard/Orders.js"
-import ViewOrder from "./Sammani/Components/AdminDashboard/ViewOrder.js"
-import Shops from "./Sammani/Components/AdminDashboard/Shops.js";
-import AddShop from "./Sammani/Components/AdminDashboard/AddShop.js";
-import UpdateShop from "./Sammani/Components/AdminDashboard/UpdateShop.js";
+import Order from "./components/sammani/AdminDashboard/Orders.js"
+import ViewOrder from "./components/sammani/AdminDashboard/ViewOrder.js"
+import Shops from "./components/sammani/AdminDashboard/Shops.js";
+import AddShop from "./components/sammani/AdminDashboard/AddShop.js";
+import UpdateShop from "./components/sammani/AdminDashboard/UpdateShop.js";
 import Mimosa from "./Sammani/Components/pages/Mimosa.js";
-import ViewContact from "./Sammani/Components/AdminDashboard/ViewContact.js";
+import ViewContact from "./components/sammani/AdminDashboard/ViewContact.js";
 import Search from "./Sammani/Components/pages/Search.js";
-import Dashboard from "./Sammani/Components/AdminDashboard/Dashboard.js";
+import Dashboard from "./components/sammani/AdminDashboard/Dashboard.js";
 import "react-toastify/dist/ReactToastify.css";
-import Sales from "./Sammani/Components/AdminDashboard/Sales.js";
+import Sales from "./components/sammani/AdminDashboard/Sales.js";
 import Footer from "./Sammani/Components/Footer.js";
 
 
